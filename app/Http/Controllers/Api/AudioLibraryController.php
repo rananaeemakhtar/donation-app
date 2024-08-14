dcece4871cf9de796d1f74214fa974afa5608954
Naeem Akhtar <rana786naeem@gmaill.com>
2024-08-14 18:55:38 +0500

    - added filters to audio library

--- app/Http/Controllers/Api/AudioLibraryController.php
+++ app/Http/Controllers/Api/AudioLibraryController.php
@@ -9,7 +9,23 @@ class AudioLibraryController extends Controller
 {
     public function index()
     {
-        $entries = AudioLibrary::all();
+        $title = request()->query('title');
+        $start_date = request()->query('from_date');
+        $end_date = request()->query('to_date');
+
+        $entries = AudioLibrary::where('audio', '!=', '')
+        ->when($title, function($q) use ($title) {
+            $q->where('title', 'LIKE', "%{$title}%");
+        })
+        ->when($start_date && $end_date, function($q) use ($start_date, $end_date) {
+            $q->whereBetween('date_of_recording', [$start_date, $end_date]);
+        })
+        ->when($start_date && !$end_date, function($q) use ($start_date) {
+            $q->where('date_of_recording', '>=', $start_date);
+        })
+        ->when(!$start_date && $end_date, function($q) use ($start_date, $end_date) {
+            $q->where('date_of_recording', '<=', $end_date);
+        })->get();
 
         return response()->json([
             'success' => true,
